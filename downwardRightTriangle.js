722f730613a7b73d0e615c52978b9a91ce079328
joshinehajoshi <neha.j@aviabird.com>
2021-08-03 00:30:06 +0530

    few more patterns

--- downwardRightTriangle.js
+++ downwardRightTriangle.js
@@ -1,13 +1,16 @@
-function downwardRightTriangle(n) {
-    let str = "";
-    for(let i=0;i<n;i++){
-        for(let j=0;j<n-i;j++){
-            str += "* "
-        }
-        for(let k=0;k<=i;k++){
-            str += " "
-        }
-        str += "\n"
+function downwardRightTrianlge(n) {
+  let str = "";
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j <= i; j++) {
+      str += " ";
     }
-    console.log(str)
+    for (let k = 0; k < n - i; k++) {
+      str += "*";
+    }
+    str += "\n";
+  }
+  console.log(str);
 }
+
+
+  
---
+++ reverseTrianlge.js
@@ -0,0 +1,17 @@
+function reverseTrianlge(n) {
+    let str = "";
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j <= i; j++) {
+        str += " ";
+      }
+      for (let k = 0; k < n - i; k++) {
+        str += "* ";
+      }
+      str += "\n";
+    }
+    console.log(str);
+  }
+  
+  
+    
+  
